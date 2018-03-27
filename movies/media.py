1ea80d845cd990891dae1d41b55655cdd098a7b6
Dragos Busuioc <dragosbusuioc94@gmail.com>
2018-03-27 07:19:53 +0000

    Init movies project

---
+++ movies/media.py
@@ -0,0 +1,6 @@
+class Movie():
+    def __init__(self, title, storyline, poster, trailer):
+        self.title = title
+        self.storyline = storyline
+        self.poster = poster
+        self.trailer = trailer
---
+++ movies/movies.py
@@ -0,0 +1,7 @@
+import media
+
+
+toy_story = media.Movie("Toy Story",
+                        "Toy Story is a computer animated film series and Disney media franchise that began with the 1995 film of the same name, produced by Pixar Animation Studios and released by Walt Disney Pictures. The franchise is based on the anthropomorphic concept that all toys, unknown to humans, are secretly alive, and the films ...",
+                        "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
